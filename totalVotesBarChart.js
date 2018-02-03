ef2c66de485dda474f2854dfbd0b423e4cd63f78
Owais <owaiswiz@gmail.com>
2018-02-03 15:17:39 +0530

    Add totalVotesBarChart

---
+++ totalVotesBarChart.js
@@ -0,0 +1,70 @@
+/* global Highcharts */
+/* global getTotalVotes */
+// Create the chart
+
+var bjpVotes = 10, congressVotes = 8, aapVotes = 9, notVotedVotes = 9;
+var totalVotes = [bjpVotes, congressVotes, aapVotes, notVotedVotes].reduce(getTotalVotes);
+
+function percentVotes(votes) {
+  return (votes/totalVotes) * 100;
+}
+Highcharts.chart('totalVotesBarChart', {
+  chart: {
+    type: 'column'
+  },
+  title: {
+    text: 'Party Wise Result'
+  },
+  xAxis: {
+    type: 'category'
+  },
+  yAxis: {
+    title: {
+      text: 'Total Percentage of Votes'
+    }
+  },
+  legend: {
+    enabled: false
+  },
+  plotOptions: {
+    series: {
+      borderWidth: 0,
+      dataLabels: {
+        enabled: true,
+        format: '{point.votes} Votes'
+      }
+    }
+  },
+
+  tooltip: {
+    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+  },
+
+  series: [{
+    name: 'Parties',
+    colorByPoint: true,
+    data: [{
+      name: 'BJP',
+      y: percentVotes(bjpVotes),
+      color: 'rgba(74,131,240,0.80)',
+      votes: bjpVotes,
+
+    }, {
+      name: 'Congress',
+      y: percentVotes(congressVotes),
+      color: 'rgba(220,71,71,0.80)',
+      votes: congressVotes,
+    }, {
+      name: 'AAP',
+      y: percentVotes(aapVotes),
+      color: 'rgba(240,190,50,0.80)',
+      votes: aapVotes,
+    }, {
+      name: 'Not Voted',
+      y: percentVotes(notVotedVotes),
+      color: 'rgba(90,200,90,0.80)',
+      votes: notVotedVotes,
+    }]
+  }],
+});
