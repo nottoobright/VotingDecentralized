6621b662d71b7e0b7622038fd90b3e0b329ec2f7
Owais <owaiswiz@gmail.com>
2018-02-04 00:39:27 +0530

    Extract vote to a modal

--- public/js/vote.js
+++ public/js/vote.js
@@ -1,7 +1,6 @@
 var stateNames = ["Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Orissa","Punjab","Rajasthan","Sikkim","Tamil Nadu","Tripura","Uttaranchal","Uttar Pradesh","West Bengal"]
-$(".submit-vote").on('click', function(e){
-    // console.log($('input[type="radio"]:checked').val());
+
+$(".submit-vote").on('click', function(){
   Voting.vote(web3.eth.accounts[ethid],$('input[type="radio"]:checked').val(),stateNames[ethid]);
   window.location.href = "/";
-  e.preventDefault();
 });
