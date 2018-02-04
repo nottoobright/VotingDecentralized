dec298594708ac8369899c1b3f0314454bc70584
Owais <owaiswiz@gmail.com>
2018-02-04 07:58:02 +0530

    Make challenge submit only once

--- public/js/challenge.js
+++ public/js/challenge.js
@@ -2,6 +2,15 @@ $("document").ready(function(){
   $(".submit-challenge").click(function(){
     var random = Math.random() >= 0.5;
     $(".challenge-before").css({'display': 'none'});
-    $(".challenge-after").css({'display': 'block'})
+    $(".submit-challenge").css({'display': 'none'});
+
+    console.log(random)
+    if (random)
+      $(".challenge-after.success").css({'display': 'block'});
+    else
+      $(".challenge-after.failed").css({'display': 'block'});
+
+    $(".challenge-system").data('toggle','');
+    $(".challenge-system .card-text.text-warning").text('Already Challeneged')
   });
 });
